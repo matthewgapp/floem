9c9378709ad892e8f358192c68e3d6be32cedcb1
Matthew Gapp <61894094+matthewgapp@users.noreply.github.com>
2023-06-12 08:11:41 -0700

    wip: commtting before changing tree type

--- examples/tree/src/main.rs
+++ examples/tree/src/main.rs
@@ -3,17 +3,23 @@ use floem::{
     reactive::{create_signal, SignalGet, SignalUpdate},
     style::Style,
     view::View,
-    views::{label, stack, tree, Decorators, ParentData},
+    views::{label, stack, tree, Decorators, ParentData, TreeData},
     ViewContext,
 };
 
 fn app_view() -> impl View {
     let cx = ViewContext::get_current();
+    let long_list: im::Vector<i32> = (0..1000000).collect();
+    let (long_list, _set_long_list) = create_signal(cx.scope, long_list);
 
-    tree(floem::views::TreeData::Parent(ParentData { 
-        child_view_fn: Box::new(|item| {
-            stack()
-                .child(label(item))
+    let view_fn = Box::new(|item| label(|| format!("Child: {}", item)));
+
+    tree(floem::views::TreeData::Parent(ParentData {
+        parent_view_fn: view_fn,
+        item_size: floem::views::VirtualListItemSize::Fixed(Box::new(|| 20.)),
+        each_fn: Box::new(|| long_list.get()),
+        key_fn: Box::new(|item| item),
+        child_view_fn: view_fn,
     }))
 }
 
